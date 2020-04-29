a09851071975d9687640e985a08e36d68bad8fe8
arifnurshobirin <arifnurshobirin@gmail.com>
2020-04-29 23:14:38 +0700

    Update content.js
    penambahan link load jd countertable

--- public/js/content.js
+++ public/js/content.js
@@ -1,49 +1,52 @@
-$(function(){
-	$('#contentdashboardv1').click(function() {
+$(function () {
+    $('#contentdashboardv1').click(function () {
         $('a').removeClass('active');
-        document.getElementById('contentdashboard').className="nav-link active";
-        document.getElementById('contentdashboardv1').className="nav-link active";
-		$('#contentpage').load('tes');
-			return false;
+        document.getElementById('contentdashboard').className = "nav-link active";
+        document.getElementById('contentdashboardv1').className = "nav-link active";
+        $('#contentpage').load('tes');
+        return false;
     })
-    $('#contentgallery').click(function() {
+    $('#contentgallery').click(function () {
         $('a').removeClass('active');
-        document.getElementById('contentgallery').className="nav-link active";
-		$('#contentpage').load('gallery');
-			return false;
+        document.getElementById('contentgallery').className = "nav-link active";
+        $('#contentpage').load('gallery');
+        return false;
     })
-    $('#contentprofile').click(function() {
-		$('#contentpage').load('profile');
-			return false;
+    $('#contentprofile').click(function () {
+        $('#contentpage').load('profile');
+        return false;
     })
-    $('#contentcontact').click(function() {
-		$('#contentpage').load('contact');
-			return false;
+    $('#contentcontact').click(function () {
+        $('#contentpage').load('contact');
+        return false;
     })
-    $('#contentedc').click(function() {
+    $('#contentedc').click(function () {
         $('#contentpage').load('edctable');
         return false;
     })
-    $('#contentcashier').click(function() {
+    $('#contentcashier').click(function () {
         $('#contentpage').load('cashiertable');
         return false;
     })
-    $('#contentmanagement').click(function() {
+    $('#contentmanagement').click(function () {
         $('#contentpage').load('managementtable');
         return false;
     })
-    $('#contentpos').click(function() {
-		$('#contentpage').load('postable');
-			return false;
+    $('#contentpos').click(function () {
+        $('#contentpage').load('postable');
+        return false;
     })
-    $('#contentcounter').click(function() {
-		$('#contentpage').load('countertable');
-			return false;
+    $('#contentcounter').click(function () {
+        $('a').removeClass('active');
+        document.getElementById('contentterminal').className = "nav-link active";
+        document.getElementById('contentcounter').className = "nav-link active";
+        $('#contentpage').load('countertable');
+        return false;
     })
-    $('#contentmonitoring').click(function() {
-		$('#contentpage').load('monitoring');
-			return false;
-	})
+    $('#contentmonitoring').click(function () {
+        $('#contentpage').load('monitoring');
+        return false;
+    })
 
 
 
