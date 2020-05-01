b4fafbb5d466f2821463c763de307c0e1caa6050
arifnurshobirin <arifnurshobirin@gmail.com>
2020-05-01 07:04:01 +0700

    Update content.js
    tambah link calendar,contact,management

--- public/js/content.js
+++ public/js/content.js
@@ -12,6 +12,12 @@ $(function () {
         $('#contentpage').load('gallery');
         return false;
     })
+    $('#contentcalendar').click(function () {
+        $('a').removeClass('active');
+        document.getElementById('contentcalendar').className = "nav-link active";
+        $('#contentpage').load('calendar');
+        return false;
+    })
     $('#contentprofile').click(function () {
         $('#contentpage').load('profile');
         return false;
@@ -21,18 +27,30 @@ $(function () {
         return false;
     })
     $('#contentedc').click(function () {
+        $('a').removeClass('active');
+        document.getElementById('contentterminal').className = "nav-link active";
+        document.getElementById('contentedc').className = "nav-link active";
         $('#contentpage').load('edctable');
         return false;
     })
     $('#contentcashier').click(function () {
+        $('a').removeClass('active');
+        document.getElementById('contentusers').className = "nav-link active";
+        document.getElementById('contentcashier').className = "nav-link active";
         $('#contentpage').load('cashiertable');
         return false;
     })
     $('#contentmanagement').click(function () {
+        $('a').removeClass('active');
+        document.getElementById('contentusers').className = "nav-link active";
+        document.getElementById('contentmanagement').className = "nav-link active";
         $('#contentpage').load('managementtable');
         return false;
     })
     $('#contentpos').click(function () {
+        $('a').removeClass('active');
+        document.getElementById('contentterminal').className = "nav-link active";
+        document.getElementById('contentpos').className = "nav-link active";
         $('#contentpage').load('postable');
         return false;
     })
