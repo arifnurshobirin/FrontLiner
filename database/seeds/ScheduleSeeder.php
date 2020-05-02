2a1a9080d1d687a53e1e03619166dfa563a60cd5
arifnurshobirin <arifnurshobirin@gmail.com>
2020-05-03 04:39:47 +0700

    Update ScheduleSeeder.php
    buat 50 field

--- database/seeds/ScheduleSeeder.php
+++ database/seeds/ScheduleSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\ScheduleModel;
 
 class ScheduleSeeder extends Seeder
 {
@@ -11,6 +12,6 @@ class ScheduleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(ScheduleModel::class, 50)->create();
     }
 }
