f2be813b102930cf57e30681ceea3121c0c37941
arifnurshobirin <arifnurshobirin@gmail.com>
2020-05-11 22:43:59 +0700

    create Monitoring
    model & controller

---
+++ app/Http/Controllers/MonitoringController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MonitoringModel;
+use App\CounterModel;
+use DB;
+use Illuminate\Http\Request;
+
+class MonitoringController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = CounterModel::all();
+        return view('monitoring.monitoring',compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MonitoringModel  $monitoringModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MonitoringModel $monitoringModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\MonitoringModel  $monitoringModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MonitoringModel $monitoringModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MonitoringModel  $monitoringModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MonitoringModel $monitoringModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MonitoringModel  $monitoringModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MonitoringModel $monitoringModel)
+    {
+        //
+    }
+}
