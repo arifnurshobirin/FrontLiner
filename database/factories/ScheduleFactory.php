db52c0a0bd17c44d797960acbb0d086db318e552
arifnurshobirin <arifnurshobirin@gmail.com>
2020-06-24 18:29:34 +0700

    Update ScheduleFactory.php
    ganti start dan end ke code shift

--- database/factories/ScheduleFactory.php
+++ database/factories/ScheduleFactory.php
@@ -5,14 +5,14 @@
 use App\ScheduleModel;
 
 
-$factory->define(ScheduleModel::class, function (Faker $faker) {
+$factory->define(ScheduleModel::class, function () {
     $faker = Faker\Factory::create('id_ID');
 
     return [
         'Employee' => $faker->unique()->numberBetween(100,999),
         'FullName' => $faker->name,
-        'Date' =>$faker->date($format = 'Y-m-d', $max = '-17 years'),
-        'StartWork' =>$faker->time($format = 'H:i', $max = 'now'),
-        'EndWork' =>$faker->time($format = 'H:i', $max = 'now'),
+        'Date' =>$faker->date($format = 'Y-m-d', $min='2020-06-01', $max = 'now'),
+        'CodeShift' =>$faker->randomElement($array = array ('OFF','A7','A8','A9','A10','A11','A12','A13','A14','A15','A16',
+                                                    'B7','B8','B9','B10','B11','B12','B13','B14','B15','B16','B17','B18'))        
     ];
 });
