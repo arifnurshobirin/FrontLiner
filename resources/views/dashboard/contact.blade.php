a7be6237e6cd98a90e878fc60cea568488d6061f
arifnurshobirin <arifnurshobirin@gmail.com>
2020-06-24 18:23:52 +0700

    Update contact.blade.php
    tambah header

--- resources/views/dashboard/contact.blade.php
+++ resources/views/dashboard/contact.blade.php
@@ -1,24 +1,4 @@
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Contacts</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Contacts</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-
-      <!-- Default box -->
+<!-- Default box -->
       <div class="card card-solid">
         <div class="card-body pb-0">
           <div class="row d-flex align-items-stretch">
@@ -329,5 +309,3 @@
       </div>
       <!-- /.card -->
 
-    </section>
-    <!-- /.content -->
