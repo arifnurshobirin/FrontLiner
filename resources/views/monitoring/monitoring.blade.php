8fee4b4d3d3905c0ada5534fb490132c7380b466
arifnurshobirin <arifnurshobirin@gmail.com>
2020-06-24 18:26:32 +0700

    Update monitoring.blade.php
    tambah warna

--- resources/views/monitoring/monitoring.blade.php
+++ resources/views/monitoring/monitoring.blade.php
@@ -1,131 +1,119 @@
-<!-- Small Box (Stat card) -->
-<h5 class="mb-2 mt-4">Small Box</h5>
-
-<div class="row"  id="accordion">
-    @foreach($data as $list)
-    <div class="col-lg-2 col-6">
-        <!-- small card -->
-        <div class="small-box bg-success">
-            <div class="inner">
-                <h3>{{ $list->NoCounter }}</h3>
-
-                <p>Reguler</p>
+<!-- Monitoring Data -->
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Monitoring</h3>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip"
+                        title="Collapse">
+                        <i class="fas fa-minus"></i></button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip"
+                        title="Remove">
+                        <i class="fas fa-times"></i></button>
+                </div>
             </div>
-            <div class="icon">
-                <i class="fas fa-cash-register"></i>
-            </div>
-            <a class="small-box-footer nav-link" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $list->id }}">
-                More info <i class="fas fa-arrow-circle-right"></i>
-            </a>
-            <!-- <a href="#" class="small-box-footer nav-link" data-toggle="dropdown">
+            <!-- /.card-header -->
+            <div class="card-body">
+                <!-- Date range -->
+                <div class="form-group">
+                    <label>Date range:</label>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <input type="text" name="fromdate" id="fromdate" class="form-control inputdaterange"
+                                placeholder="From Date" readonly />
+                        </div>
+                        <div class="col-md-4 todate2">
+                            <input type="text" name="todate" id="todate" class="form-control todate"
+                                placeholder="To Date" readonly />
+                        </div>
+                        <div class="col-md-4">
+                            <button type="button" name="scheduleapply" id="scheduleapply"
+                                class="btn btn-primary">Apply</button>
+                            <button type="button" name="refresh" id="refresh" class="btn btn-default">Refresh</button>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.form group -->
+                <br>
+                <div class="row">
+                    @foreach($data as $list)
+                    <div class="col-lg-2 col-6">
+                        <!-- small card -->
+                        @if($list->StatusCounter == 'Queueing')
+                        <div id="changecolor" class="small-box bg-danger">
+                        @elseif($list->StatusCounter == 'Active')
+                        <div id="changecolor" class="small-box bg-success">
+                        @elseif($list->StatusCounter == 'Inactive')
+                        <div id="changecolor" class="small-box bg-warning">
+                        @elseif($list->StatusCounter == 'Ready')
+                        <div id="changecolor" class="small-box bg-secondary">
+                        @elseif($list->StatusCounter == 'Broken')
+                        <div id="changecolor" class="small-box bg-black">
+                        @endif
+                            <div class="inner">
+                                <h3>{{ $list->NoCounter }}</h3>
+
+                                <p>{{ $list->TypeCounter }}</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-cash-register"></i>
+                            </div>
+                            <a class="small-box-footer nav-link" data-toggle="collapse" data-parent="#accordion"
+                                href="#collapse{{ $list->id }}">
+                                More info <i class="fas fa-arrow-circle-right"></i>
+                            </a>
+                            <!-- <a href="#" class="small-box-footer nav-link" data-toggle="dropdown">
                     More info <i class="fas fa-arrow-circle-right"></i>
                 </a>
                         <div class="box-body bg-white dropdown-menu">
                         <button class="btn-primary dropdown-item">Change Color</button>
                         </div> -->
-            <div id="collapse{{ $list->id }}" class="panel-collapse collapse in">
-                <div class="card-body">
-                    Anim pariatur   
+                            <div id="collapse{{ $list->id }}" class="panel-collapse collapse in">
+                                <div class="card-body">
+                                    Anim pariatur
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                    Project Website Cashier Carrefour Taman Palem
                 </div>
             </div>
         </div>
+        <!-- /.card -->
     </div>
-@endforeach
+    <!-- /.col -->
 </div>
 <!-- /.row -->
 
 
-<div  id="monitoringcontainer">
-
-</div>
-
-
 <script>
-    
-$(document).ready(function () {
-    var allCounter = < ? php echo $data ? > ;
-    var counterContent = "";
-    var lastIndex;
-    for (var i = 0; i < allCounter.length; i++) {
-        // for(var i=0;i<18;i++){
-        lastIndex = i;
-        if ((i % 6) == 0) {
-            if (i == 0) {
-                counterContent = counterContent + "<div class='row'>";
-            } else {
-                counterContent = counterContent + "</div>";
-                counterContent = counterContent + "<div class='row'>";
-            }
+    $(document).ready(function() {
+        // var datapos = new Array();
+        // var status = $(this).attr('list->StatusCounter');
 
+        // conslole log('StatusCounter');
+        // if (status === 'Queueing') {
+        //     $("#changecolor").removeClass("bg-secondary");
+        //     $("#changecolor").addClass("bg-danger");
+        //     }
+        // else if (status === 'Active') {
+        //     $("#changecolor").removeClass("bg-secondary");
+        //     $("#changecolor").addClass("bg-success");
+        //     // $( ".bg-success" ).switchClass( "bg-success", "bg-info", 1000 );
+        //     }
+        // else if (status === 'Inaktive') {
+        //     $("#changecolor").removeClass("bg-secondary");
+        //     $("#changecolor").addClass("bg-warning");
+        //     }
+        // else if (status === 'Broken') {
+        //     $("#changecolor").removeClass("bg-secondary");
+        //     $("#changecolor").addClass("bg-black");
+        //     }
 
-            lastIndex = 0;
-        }
-        counterContent = counterContent + "<div class='col-lg-2 col-6'>";
-        counterContent = counterContent + "<div class='datapos small-box " + allCounter[i].status_monitoring + "'  id='counter_" + allCounter[i].id_counter + "' >";
-        counterContent = counterContent + "<div class='inner'>";
-        counterContent = counterContent + "<h5>" + allCounter[i].namaCounter + "</h5>";
-        counterContent = counterContent + "<h6>" + allCounter[i].fullname + "</h6>";
-        counterContent = counterContent + "</div>";
-        counterContent = counterContent + " <div class='icon'>";
-        counterContent = counterContent + " <i class='ion ion-person' nama='gantiwarna' id='gantiwarna' ></i>";
-        counterContent = counterContent + " </div>";
-        counterContent = counterContent + " <div class='box box-default collapsed-box box-solid'>";
-        counterContent = counterContent + " <a class='small-box-footer '>More info<button class='btn btn-box-tool' data-widget='collapse'><i class='fa fa-arrow-circle-right'></i></button></a>";
-        counterContent = counterContent + "  <div class='box-body bg-white'>";
-        counterContent = counterContent + " <button class='btn-primary btn-block btn-sm' onClick='changeColor(" + allCounter[i].id_counter + ")'>Change Color</button>";
-        counterContent = counterContent + " </div>";
-        counterContent = counterContent + " </div>";
-        counterContent = counterContent + " </div>";
-        counterContent = counterContent + " </div>";
-
-
-
-
-
-    }
-    if ((lastIndex % 6) != 0) {
-        // counterContent = counterContent+"</div>";
-    }
-
-    $("#counterContainer").html(counterContent);
-
-
-});
-
-function changeColor(id) {
-    if ($("#counter_" + id).hasClass("bg-green")) {
-        $("#counter_" + id).removeClass("bg-green");
-        $("#counter_" + id).addClass("bg-red");
-    } else if ($("#counter_" + id).hasClass("bg-red")) {
-        $("#counter_" + id).removeClass("bg-red");
-        $("#counter_" + id).addClass("bg-black");
-    } else if ($("#counter_" + id).hasClass("bg-black")) {
-        $("#counter_" + id).removeClass("bg-black");
-        $("#counter_" + id).addClass("bg-gray");
-    } else if ($("#counter_" + id).hasClass("bg-gray")) {
-        $("#counter_" + id).removeClass("bg-gray");
-        $("#counter_" + id).addClass("bg-green");
-    } else {
-        $("#counter_" + id).addClass("bg-green");
-    }
-}
-
-
-$("#btnSaveMonitoring").click(function () {
-
-    var datapos = new Array();
-    $(".datapos").each(function (idx, elm) {
-        datapos.push({
-            'id': $(elm).attr("id"),
-            'class': $(elm).attr("class")
-        });
-    })
-
-
-    $.post("save-monitoring", "datapos=" + JSON.stringify(datapos), function () {
-        alert("Berhasil Save POS");
-    })
-
-})
-
+    });
 </script>
