85ce835983b6e20f9f4a64bc32cb8ac230849501
arifnurshobirin <arifnurshobirin@gmail.com>
2020-07-20 18:30:17 +0700

    Update EDCdatatable.blade.php
    tambah preload

--- resources/views/edc/EDCdatatable.blade.php
+++ resources/views/edc/EDCdatatable.blade.php
@@ -1,6 +1,14 @@
 @include('sweetalert::alert')
 <!-- EDC Table -->
 <div class="row">
+    <div class="preloader">
+        <div class="loading">
+            <div class="spinner-grow text-danger" role="status"></div>
+            <div class="spinner-grow text-danger" role="status"></div>
+            <div class="spinner-grow text-danger" role="status"><span class="sr-only">Loading...</span></div>
+            <strong>Loading...</strong>
+        </div>
+    </div>
     <div class="col-12">
         <div class="card">
             <div class="card-header">
@@ -16,20 +24,15 @@
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-                <div>
-                    <button type="button" name="edccreate" id="edccreate" class="btn btn-success waves-effect">
-                        <i class="fas fa-plus"></i><span> Add EDC</span>
-                    </button>
-                </div>
-                <br>
                 <div class="table-responsive">
                     <table class="table table-bordered table-striped table-hover dataTable js-exportable"
                         id="EDCDatatable">
                         <thead>
                             <tr>
-                                <th><button type="button" name="edcsomedelete" id="edcsomedelete" class="btn btn-danger">
+                                <th><button type="button" name="edcmoredelete" id="edcmoredelete" class="btn btn-danger">
                                 <i class="fas fa-times"></i><span></span>
                                 </button></th>
+                                <th></th>
                                 <th>TID EDC</th>
                                 <th>No Counter</th>
                                 <th>Connection</th>
@@ -40,7 +43,8 @@
                         </thead>
                         <tfoot>
                             <tr>
-                                <th>Checkbox</th>
+                                <th></th>
+                                <th></th>
                                 <th>TID EDC</th>
                                 <th>No Counter</th>
                                 <th>Connection</th>
@@ -135,6 +139,19 @@
                             </select>
                         </div>
                     </div>
+                    <label for="type">Status EDC</label>
+                    <div class="form-group">
+                        <div class="form-line">
+                            <select class="form-control show-tick" id="satusedc" name="statusedc">
+                                <option value="">-- Please select --</option>
+                                <option value="Active">Active</option>
+                                <option value="Inactive">Inactive</option>
+                                <option value="Normal">Normal</option>
+                                <option value="Broken">Broken</option>
+                                <option value="Queueing">Queueing</option>
+                            </select>
+                        </div>
+                    </div>
                     <button type="submit" class="btn btn-primary m-t-15 waves-effect" id="saveBtn"
                         value="create">Save</button>
                 </form>
@@ -145,6 +162,23 @@
 <!-- #END# Create Table -->
 
 <script>
+function format ( d ) {
+    // `d` is the original data object for the row
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+        '<tr>'+
+            '<td>Full name:</td>'+
+            '<td>'+d.TypeEDC+'</td>'+
+        '</tr>'+
+        '<tr>'+
+            '<td>Extension number:</td>'+
+            '<td>'+d.Connection+'</td>'+
+        '</tr>'+
+        '<tr>'+
+            '<td>Extra info:</td>'+
+            '<td>And any further details here (images etc)...</td>'+
+        '</tr>'+
+    '</table>';
+}
     $(document).ready(function() {
         var table = $('#EDCDatatable').DataTable({
             processing: true,
@@ -152,25 +186,72 @@
             ajax: {
             url:"{{ route('edc.index') }}",
             },
-            "order": [[ 1, "asc" ]],
+            "order": [[ 2, "asc" ]],
             columns: [
                 { data: 'checkbox', name: 'checkbox', orderable:false, searchable: false},
+                {
+                "className":      'details-control',
+                "orderable":      false,
+                "searchable":     false,
+                "data":           null,
+                "defaultContent": ''
+                },
                 { data: 'TIDEDC', name: 'TIDEDC' },
                 { data: 'NoCounter', name: 'NoCounter' },
                 { data: 'Connection', name: 'Connection' },
                 { data: 'SIMCard', name: 'SIMCard' },
                 { data: 'TypeEDC', name: 'TypeEDC' },
-                { data: 'action', name: 'action', orderable: false}
-            ]
+                { data: 'action', name: 'action', orderable: false,searchable: false}
+            ],
+            dom: 'Bfrtip',
+        lengthMenu: [
+            [ 10, 25, 50, -1 ],
+            [ '10 rows', '25 rows', '50 rows', 'Show all' ]
+        ],
+        buttons:['pageLength',
+                    
+                    {
+                        extend: 'collection',
+                        text: 'Export',
+                        className: 'btn btn-info',
+                        buttons:[ 'copy', 'csv', 'excel', 'pdf', 'print',
+                                    {
+                                        collectionTitle: 'Visibility control',
+                                        extend: 'colvis',
+                                        collectionLayout: 'two-column'
+                                    }
+                                ]
+                    },
+                    {
+                        text: '<i class="fas fa-plus"></i><span> Add EDC</span>',
+                        className: 'btn btn-success',
+                        action: function ( e, dt, node, config ) {
+                            $('#saveBtn').val("create-edc");
+                            $('#saveBtn').html('Save');
+                            $('#edcid').val('');
+                            $('#edcForm').trigger("reset");
+                            $('#modelHeading').html("Create New EDC");
+                            $('#ajaxModel').modal('show');
+                        }
+                    }
+                ]
         });
 
-        $('#edccreate').click(function () {
-            $('#saveBtn').val("create-edc");
-            $('#saveBtn').html('Save');
-            $('#edcid').val('');
-            $('#edcForm').trigger("reset");
-            $('#modelHeading').html("Create New EDC");
-            $('#ajaxModel').modal('show');
+        // Add event listener for opening and closing details
+        $('#EDCDatatable').on('click', 'td.details-control', function () {
+            var tr = $(this).closest('tr');
+            var row = table.row( tr );
+
+            if ( row.child.isShown() ) {
+                // This row is already open - close it
+                row.child.hide();
+                tr.removeClass('shown');
+            }
+            else {
+                // Open this row
+                row.child( format(row.data()) ).show();
+                tr.addClass('shown');
+            }
         });
 
         $(document).on('click', '.editedc', function () {
@@ -211,7 +292,7 @@
                     $('#ajaxModel').modal('hide');
                     $('#saveBtn').html('Save');
                     table.draw();
-                    showSuccessMessage();
+                    swal.fire("Good job!", "You success update EDC!", "success");
                 },
                 error: function (data) {
                     console.log('Error:', data);
@@ -222,13 +303,8 @@
 
         var type;
         var edcid;
-        $(document).on('click', '.js-sweetalert', function(){
+        $(document).on('click', '.deleteedc', function(){
             edcid = $(this).attr('id');
-            showDeleteTable();
-        });
-
-
-        function showDeleteTable() {
             swal.fire({
                 title: "Are you sure?",
                 text: "You will not be able to recover this edc file!",
@@ -250,11 +326,44 @@
                     swal.fire("Cancelled", "Your edc file is safe :)", "error");
                 }
             });
-        }   
-        function showSuccessMessage() {
-            swal.fire("Good job!", "You success update EDC!", "success");
-        }
+        });
+
+        $(document).on('click', '#edcmoredelete', function(){
+            var id = [];
+            swal.fire({
+                title: "Are you sure?",
+                text: "You will not be able to recover this EDC file!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, delete!",
+                cancelButtonText: "No, cancel!"
+            }).then((result) => {
+                if (result.value) {
+                    $('.edccheckbox:checked').each(function(){
+                        id.push($(this).val());
+                    });
+                    if(id.length > 0)
+                    {
+                        $.ajax({
+                        url:"{{ route('edc.moredelete')}}",
+                        method:"get",
+                        data:{id:id},
+                        success:function(data){
+                        swal.fire("Deleted!", "Your EDC file has been deleted.", "success")
+                        $('#EDCDatatable').DataTable().ajax.reload();
+                            }
+                        });
+                    }
+                    else
+                    {swal.fire("Please select atleast one checkbox");}
+                } 
+                else 
+                {swal.fire("Cancelled", "Your EDC file is safe :)", "error");}
+                });
+            });
 
     });
+    $(".preloader").fadeOut("slow");
 
 </script>
