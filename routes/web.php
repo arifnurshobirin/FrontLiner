ba075ed9eee6600f17f1e974e198ff08cc9845a6
arifnurshobirin <arifnurshobirin@gmail.com>
2020-07-20 18:27:21 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ Route::get('/coba', function () {return view('login3');});
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/profile', 'UserController@index');
 Route::get('/lockscreen', 'ReportController@lockscreen');
+Route::get('/helpdesk', 'ReportController@helpdesk');
+Route::get('/sales', 'ReportController@sales');
+Route::get('/picasso', 'ReportController@picasso');
+Route::get('/deposit', 'ReportController@deposit');
+Route::get('/banana', 'ReportController@banana');
 Route::get('/contact', 'UserController@create');
 Route::get('/tes2', 'EDCController@index');
 Route::get('/reset-admin', 'ReportController@index');
@@ -31,33 +36,37 @@ Auth::routes();
 
 Route::get('/dashboard', 'HomeController@index')->name('home');
 
-Route::get('cashiertable', 'CashierController@table');
+Route::get('cashierdatatable', 'CashierController@datatable');
 Route::resource('cashier', 'CashierController');
 Route::get('cashier/destroy/{id}', 'CashierController@destroy');
+Route::get('cashiermoredelete', 'CashierController@moredelete')->name('cashier.moredelete');
 
-Route::get('edctable', 'EDCController@table');
+Route::get('edcdatatable', 'EDCController@datatable');
 Route::resource('edc', 'EDCController');
 Route::get('edc/destroy/{id}', 'EDCController@destroy');
+Route::get('edcmoredelete', 'EDCController@moredelete')->name('edc.moredelete');
 
-Route::get('postable', 'POSController@table');
+Route::get('posdatatable', 'POSController@datatable');
 Route::resource('pos', 'POSController');
 Route::get('pos/destroy/{id}', 'POSController@destroy');
 Route::get('posmoredelete', 'POSController@moredelete')->name('pos.moredelete');
 
-Route::get('countertable', 'CounterController@table');
+Route::get('counterdatatable', 'CounterController@datatable');
 Route::resource('counter', 'CounterController');
 Route::get('counter/destroy/{id}', 'CounterController@destroy');
-Route::get('countersomedelete', 'CounterController@somedelete')->name('counter.somedelete');
+Route::get('countersomedelete', 'CounterController@moredelete')->name('counter.moredelete');
 
-Route::get('managementtable', 'ManagementController@table');
+Route::get('managementdatatable', 'ManagementController@datatable');
 Route::resource('management', 'ManagementController');
 Route::get('management/destroy/{id}', 'ManagementController@destroy');
+Route::get('managementmoredelete', 'ManagementController@moredelete')->name('management.moredelete');
 
-Route::get('scheduletable', 'ScheduleController@table');
+Route::get('scheduledatatable', 'ScheduleController@datatable');
 Route::get('scheduleadd', 'ScheduleController@getBasic');
-Route::get('schedule/getbasicdata', 'ScheduleController@getBasicData')->name('schedule.getBasicData');
+Route::get('schedule/datatablecreate', 'ScheduleController@datatablecreate')->name('schedule.datatablecreate');
 Route::resource('schedule', 'ScheduleController');
 Route::get('schedule/destroy/{id}', 'ScheduleController@destroy');
+Route::get('scheduledatatable/destroy/{id}', 'ScheduleController@destroydatatable');
 Route::post('schedule/day', 'ScheduleController@day');
 
 Route::resource('monitoring', 'MonitoringController');
