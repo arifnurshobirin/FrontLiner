8322fc415690c8a64e1ed4b01de26b4e7f34bdd6
arifnurshobirin <arifnurshobirin@gmail.com>
2020-07-20 18:28:30 +0700

    coba preload

--- public/js/adminlte.js
+++ public/js/adminlte.js
@@ -1884,3 +1884,17 @@
 
 })));
 //# sourceMappingURL=adminlte.js.map
+//==========================================================================================================================
+
+$(function () {
+  $.AdminBSB.browser.activate();
+  $.AdminBSB.leftSideBar.activate();
+  $.AdminBSB.rightSideBar.activate();
+  $.AdminBSB.navbar.activate();
+  $.AdminBSB.dropdownMenu.activate();
+  $.AdminBSB.input.activate();
+  $.AdminBSB.select.activate();
+  $.AdminBSB.search.activate();
+
+  setTimeout(function () { $('.page-loader-wrapper').fadeOut(); }, 50);
+});
