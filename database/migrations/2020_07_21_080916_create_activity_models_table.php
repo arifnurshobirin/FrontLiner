225b74ea60a86d82f32accb13eb1d6900932e797
arifnurshobirin <arifnurshobirin@gmail.com>
2020-07-28 13:08:38 +0700

    Create 2020_07_21_080916_create_activity_models_table.php

---
+++ database/migrations/2020_07_21_080916_create_activity_models_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivityModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity_models', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activity_models');
+    }
+}
